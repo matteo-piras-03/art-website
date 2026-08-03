015e02e8fc7d7b69dc53b2a26e20d573f8693b1e
matteo-piras-03 <piras.unict@gmail.com>
2026-08-03 17:07:51 +0200

    03-08

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'home']);
+Route::get('/art', [PageController::class, 'art']);
+Route::get('/blog', [PageController::class, 'blog']);
+Route::get('/warmups', [PageController::class, 'warmups']);
