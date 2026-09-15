ed058e590f13a522214eac113f6bb2b4604fc505
piras-unict <prsmtt03m16c351z@studium.unict.it>
2026-09-15 09:48:34 +0200

    New drawing, slicing exercise

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ const img_list = [
     "https://media.piras03.com/image/af2026/09.jpg",
     "https://media.piras03.com/image/af2026/07.jpg",
     "https://media.piras03.com/image/dabcelebration/05.jpg",
-    "https://media.piras03.com/image/af2026/05.jpg",
+    "https://media.piras03.com/image/digitalvol2/07.jpg",
     "https://media.piras03.com/image/af2026/03.jpg",
     "https://media.piras03.com/image/af2026/04.jpg",
     "https://media.piras03.com/image/af2026/01.jpg",
